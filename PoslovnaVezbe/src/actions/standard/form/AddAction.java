b13da98f37009659a788f689e031fcada155768d
DejanUrosevic <dejansuca@gmail.com>
2016-04-26 21:46:42 +0200

    proba2lkas

--- PoslovnaVezbe/src/actions/standard/form/AddAction.java
+++ PoslovnaVezbe/src/actions/standard/form/AddAction.java
@@ -23,6 +23,12 @@ public class AddAction extends AbstractAction {
 		this.standardForm=standardForm;
 	}
 
+	/*
+	 * PROBA
+	 * 
+	 * (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		//((DrzavaStandardForm)standardForm).getTfNaziv().setText("");
